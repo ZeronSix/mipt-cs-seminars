9ac61389b3724367ceae978f52cbde07beeeda89
Vyacheslav Zhdanovskiy <zeronsix@gmail.com>
2017-03-08 18:27:26 +0300

    Add first task

---
+++ task1/task1.asm
@@ -0,0 +1,17 @@
+section .text
+        global  main
+main:
+        mov     rax, 1          ; syscall 1 is write
+        mov     rdi, 1          ; file handle 1 is stdout
+        mov     rsi, msg        ; pointer to output string
+        mov     rdx, msg_len    ; output string length
+        syscall             
+
+        mov     rax, 60         ; syscall 60 is exit
+        mov     rdi, 0          ; exit code 0
+        syscall
+
+section .data
+msg     db      "Hello, world!", 10
+msg_len equ     $-msg               ; len = strlen(msg);
+
