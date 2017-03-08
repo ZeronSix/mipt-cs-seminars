a5e3a7ccbee27ff15a4a83c7403f56394596041b
Vyacheslav Zhdanovskiy <zeronsix@gmail.com>
2017-03-09 00:17:23 +0300

    Add task 4

---
+++ task4/task4.asm
@@ -0,0 +1,102 @@
+; task3.asm     Contains functions that convert numbers
+;               to their string representations, but uses
+;               stack frames
+
+section .bss
+buffer  resb    64               ; output buffer
+
+
+section .text
+        global _start
+
+_start:
+        push    5234
+        call    print_dec_stackframed
+
+        mov     rax, 60         ; syscall <- 60 (exit)         
+        mov     rdi, 0          ; exit(0)
+        syscall
+
+;---------------------------------------|
+; print_char - prints a character.      |
+;---------------------------------------|
+; Entry - RBX (character)               |
+; Exit - None                           |
+; Destr - RDI, RSI, RDX, RAX            |
+;---------------------------------------|
+print_char:
+        push    rbx
+        mov     rax, 1         ; syscall <- 1
+        mov     rdi, 1         ; fd      <- stdout(1)
+        mov     rsi, rsp       ; buf     <- [rsp]
+        mov     rdx, 1         ; count   <- 1
+        pop     rbx
+        syscall 
+        ret
+
+;---------------------------------------|
+; print_dec - converts a number to its  |
+;              string representation.   |
+;---------------------------------------|
+; Entry - RAX (number)                  |
+; Exit  - none                          |
+; Destr - R8, R9, RDX, RAX, RBX         |
+;         RDI, RSI, RDX, RAX            |
+;---------------------------------------|
+print_dec:
+        mov     r8, 0
+        mov     r9, 10           ; base = 10
+
+.loop_begin:
+        xor     rdx, rdx         ; rdx = 0
+        div     r9               ; rax /= 10 [rdx %= 10]
+        add     rdx, '0'         ; rdx += '0'
+        mov     [buffer+r8], rdx ; rbx = rdx
+        inc     r8               ; i++
+        cmp     rax, 0           ; if (r8 > 0)
+        je      .loop2_begin
+        jmp     .loop_begin
+.loop2_begin:
+        mov     rbx, [buffer+r8]
+        call    print_char       ; print_char(buffer[i])
+        cmp     r8, 0
+        je      .loop2_end
+        dec     r8
+        jmp     .loop2_begin
+.loop2_end:
+        mov     rbx, `\n`
+        call    print_char
+
+        ret
+
+;--------------------------------------|
+; print_dec_stackframed - stackframe   |
+;                         wrapper      |
+;--------------------------------------|
+; Entry - stack (number)               |
+; Exit - none                          |
+; Destr - none                         |
+;--------------------------------------|
+print_dec_stackframed:
+        push rbp
+        push r8
+        push r9
+        push rax
+        push rbx
+        push rdx
+        push rsi
+        push rdi
+        mov rbp, rsp
+
+        mov rax, [rbp + 8 * 9]
+        call print_dec
+
+        pop rdi
+        pop rsi
+        pop rdx
+        pop rbx
+        pop rax
+        pop r9
+        pop r8
+        pop rbp
+        ret
