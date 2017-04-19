4e4be5798ff299b0eb49413864c9351891705e91
Vyacheslav Zhdanovskiy <zeronsix@gmail.com>
2017-04-19 23:07:24 +0300

    Inline hash function and strcmp

--- optimization/hashtable_optimized.c
+++ optimization/hashtable_optimized.c
@@ -5,38 +5,41 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define INLINE_HASH(label) \
+        "mov eax, 0\n\t" \
+        "xor r8, r8\n\t" \
+        "loop" label "_cond:\n\t" \
+        "xor r8, r8\n\t" \
+        "movzx r8, byte ptr [rdi]\n\t"   \
+        "test r8b, r8b\n\t" \
+        "je loop" label "_end\n\t" \
+        "add eax, r8d\n\t" \
+        "mov r8d, eax\n\t" \
+        "sal r8d, 10\n\t" \
+        "add eax, r8d\n\t" \
+        "mov r8d, eax\n\t" \
+        "shr r8d, 6\n\t" \
+        "xor eax, r8d\n\t" \
+        "inc rdi\n\t" \
+        "jmp loop" label "_cond\n\t" \
+        "loop" label "_end:\n\t" \
+        "mov r8d, eax\n\t" \
+        "sal r8d, 3\n\t" \
+        "add eax, r8d\n\t" \
+        "mov r8d, eax\n\t" \
+        "shr r8d, 11\n\t" \
+        "xor eax, r8d\n\t" \
+        "mov r8d, eax\n\t" \
+        "sal r8d, 15\n\t" \
+        "add eax, r8d\n\t" \
+        "mov ebx, 3007\n\t" \
+        "xor edx, edx\n\t" \
+        "div ebx\n\t" \
+        "mov eax, edx\n\t" 
+
 static unsigned hash(const char* str) {
     asm(".intel_syntax noprefix\n\t"
-        "mov eax, 0\n\t"
-        "mov r8, 0\n\t"
-        "loop_cond:\n\t"
-        "mov r8, 0\n\t"
-        "movzx r8, byte ptr [rdi]\n\t"  
-        "test r8b, r8b\n\t"
-        "je loop_end\n\t"
-        "add eax, r8d\n\t"
-        "mov r8d, eax\n\t"
-        "sal r8d, 10\n\t"
-        "add eax, r8d\n\t"
-        "mov r8d, eax\n\t"
-        "shr r8d, 6\n\t"
-        "xor eax, r8d\n\t"
-        "inc rdi\n\t"
-        "jmp loop_cond\n\t"
-        "loop_end:\n\t"
-        "mov r8d, eax\n\t"
-        "sal r8d, 3\n\t"
-        "add eax, r8d\n\t"
-        "mov r8d, eax\n\t"
-        "shr r8d, 11\n\t"
-        "xor eax, r8d\n\t"
-        "mov r8d, eax\n\t"
-        "sal r8d, 15\n\t"
-        "add eax, r8d\n\t"
-        "mov ebx, 3007\n\t"
-        "mov edx, 0\n\t"
-        "div ebx\n\t"
-        "mov eax, edx\n\t"
+        INLINE_HASH("1")
         :
         :
         : "%eax", "%ecx", "%r8", "%rdi", "%edx", "%ebx"
@@ -48,7 +51,9 @@ HtItem* ht_search(HashTable* ht, const char* s) {
         "mov r9, rdi\n\t" // move ht to r9
         "mov r10, rsi\n\t" // move s to r10
         "mov rdi, r10\n\t"
-        "call hash\n\t"
+
+        INLINE_HASH("2")
+        
         "mov rax, [r9 + 8 * rax]\n\t"
         "searchloop_cond:\n\t"
         "test rax, rax\n\t"
@@ -56,7 +61,19 @@ HtItem* ht_search(HashTable* ht, const char* s) {
         "mov rdi, r10\n\t"
         "mov rsi, [rax+8]\n\t"
         "mov r11, rax\n\t"
-        "call strcmp@PLT\n\t"
+        // strcmp
+        "xor r12, r12\n\t"
+        "strcmp_cond:\n\t"
+        "mov rax, 1\n\t"
+        "mov r13, [rdi + r12]\n\t"
+        "mov r14, [rsi + r12]\n\t"
+        "cmp r13, r14\n\t"
+        "jne strcmp_end\n\t"
+        "inc r12\n\t"
+        "cmp r13, 0\n\t"
+        "jne  strcmp_cond\n\t"
+        "mov rax, 0\n\t"
+        "strcmp_end:\n\t"
         "mov esi, eax\n\t"
         "mov rax, r11\n\t"
         "test esi, esi\n\t"
@@ -66,7 +83,7 @@ HtItem* ht_search(HashTable* ht, const char* s) {
         "searchloop_end:\n\t"
         :
         :
-        : "%rax", "%r9", "%r10", "%rdi", "%rsi", "%r11"
+        : "%rax", "%r9", "%r10", "%rdi", "%rsi", "%r11", "%r12", "%r13", "%r14", "%rcx", "%rdx"
     );
 }
 
--- optimization/hashtable_optimized.s
+++ optimization/hashtable_optimized.s
@@ -14,15 +14,15 @@ hash:
 	.cfi_offset 3, -24
 	mov	QWORD PTR -16[rbp], rdi
 #APP
-# 9 "hashtable_optimized.c" 1
+# 41 "hashtable_optimized.c" 1
 	.intel_syntax noprefix
 	mov eax, 0
-	mov r8, 0
-	loop_cond:
-	mov r8, 0
+	xor r8, r8
+	loop1_cond:
+	xor r8, r8
 	movzx r8, byte ptr [rdi]
 	test r8b, r8b
-	je loop_end
+	je loop1_end
 	add eax, r8d
 	mov r8d, eax
 	sal r8d, 10
@@ -31,8 +31,8 @@ hash:
 	shr r8d, 6
 	xor eax, r8d
 	inc rdi
-	jmp loop_cond
-	loop_end:
+	jmp loop1_cond
+	loop1_end:
 	mov r8d, eax
 	sal r8d, 3
 	add eax, r8d
@@ -43,7 +43,7 @@ hash:
 	sal r8d, 15
 	add eax, r8d
 	mov ebx, 3007
-	mov edx, 0
+	xor edx, edx
 	div ebx
 	mov eax, edx
 	
@@ -67,15 +67,50 @@ ht_search:
 	.cfi_offset 6, -16
 	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	mov	QWORD PTR -8[rbp], rdi
-	mov	QWORD PTR -16[rbp], rsi
+	push	r14
+	push	r13
+	push	r12
+	.cfi_offset 14, -24
+	.cfi_offset 13, -32
+	.cfi_offset 12, -40
+	mov	QWORD PTR -32[rbp], rdi
+	mov	QWORD PTR -40[rbp], rsi
 #APP
-# 47 "hashtable_optimized.c" 1
+# 50 "hashtable_optimized.c" 1
 	.intel_syntax noprefix
 	mov r9, rdi
 	mov r10, rsi
 	mov rdi, r10
-	call hash
+	mov eax, 0
+	xor r8, r8
+	loop2_cond:
+	xor r8, r8
+	movzx r8, byte ptr [rdi]
+	test r8b, r8b
+	je loop2_end
+	add eax, r8d
+	mov r8d, eax
+	sal r8d, 10
+	add eax, r8d
+	mov r8d, eax
+	shr r8d, 6
+	xor eax, r8d
+	inc rdi
+	jmp loop2_cond
+	loop2_end:
+	mov r8d, eax
+	sal r8d, 3
+	add eax, r8d
+	mov r8d, eax
+	shr r8d, 11
+	xor eax, r8d
+	mov r8d, eax
+	sal r8d, 15
+	add eax, r8d
+	mov ebx, 3007
+	xor edx, edx
+	div ebx
+	mov eax, edx
 	mov rax, [r9 + 8 * rax]
 	searchloop_cond:
 	test rax, rax
@@ -83,7 +118,18 @@ ht_search:
 	mov rdi, r10
 	mov rsi, [rax+8]
 	mov r11, rax
-	call strcmp@PLT
+	xor r12, r12
+	strcmp_cond:
+	mov rax, 1
+	mov r13, [rdi + r12]
+	mov r14, [rsi + r12]
+	cmp r13, r14
+	jne strcmp_end
+	inc r12
+	cmp r13, 0
+	jne  strcmp_cond
+	mov rax, 0
+	strcmp_end:
 	mov esi, eax
 	mov rax, r11
 	test esi, esi
@@ -95,6 +141,9 @@ ht_search:
 # 0 "" 2
 #NO_APP
 	nop
+	pop	r12
+	pop	r13
+	pop	r14
 	pop	rbp
 	.cfi_def_cfa 7, 8
 	ret
